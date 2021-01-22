80c18ee8255e2f71afe4c86b92eeae11a74fcf2e
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2021-01-22 09:23:43 -0600

    Correct files_information.py when getting files in subdir.

--- tests/test_files_information.py
+++ tests/test_files_information.py
@@ -1,6 +1,9 @@
 import unittest
 from zeppos_file_manager.files_information import FilesInformation
 from tests.util_for_testing import UtilForTesting
+import shutil
+import os
+
 
 class TestTheProjectMethods(unittest.TestCase):
     def setUp(self):
@@ -10,28 +13,56 @@ class TestTheProjectMethods(unittest.TestCase):
         UtilForTesting.file_clean_up()
 
     def test_get_files_by_extension_method(self):
-        FilesInformation.get_files_by_extension("c:\\")
-        FilesInformation.get_files_by_extension("c:\\", "*.txt")
-        FilesInformation.get_files_by_extension("c:\\", "*.txt", "test1", "test2")
-        FilesInformation.get_files_by_extension("c:\\", "*.txt", "test1", "test2", True)
-        FilesInformation.get_files_by_extension(base_dir="c:\\")
+        temp_dir, file_dir, full_file_name_list = UtilForTesting.file_setup(
+            r'test_1', extension="",
+            content="col1,col2\ntest1,test2")
+        files = FilesInformation.get_files_by_extension(base_dir=file_dir, extension="csv")
+        self.assertEqual(1, len(files))
+
+    def test_get_files_by_extension_start_file_filter_method(self):
+        temp_dir, file_dir, full_file_name_list = UtilForTesting.file_setup(
+            r'test_2', extension="",
+            content="col1,col2\ntest1,test2")
+        shutil.move(full_file_name_list[0], os.path.join(file_dir, 'testme_test_0.csv'))
+        files = FilesInformation.get_files_by_extension(base_dir=file_dir, extension="csv", start_file_filter="testme")
+        self.assertEqual(1, len(files))
+
+    def test_get_files_by_extension_end_file_filter_method(self):
+        temp_dir, file_dir, full_file_name_list = UtilForTesting.file_setup(
+            r'test_3', extension="",
+            content="col1,col2\ntest1,test2")
+        shutil.move(full_file_name_list[0], os.path.join(file_dir, 'test_0_testme.csv'))
+        files = FilesInformation.get_files_by_extension(base_dir=file_dir, extension="csv", end_file_filter="testme")
+        self.assertEqual(1, len(files))
 
     def test_get_files_by_extension_include_subdir_method(self):
         temp_dir, file_dir, full_file_name_list = UtilForTesting.file_setup(
-            r'test_1\field1=somedata1\field2=somedata2', extension="",
+            r'test_4\field1=somedata1\field2=somedata2', extension="",
             content="col1,col2\ntest1,test2")
-        files = FilesInformation.get_files_by_extension(base_dir=file_dir, extension="*.csv", include_subdir=True)
+        files = FilesInformation.get_files_by_extension(base_dir=temp_dir, extension="csv", include_subdir=True)
+        self.assertEqual(1, len(files))
+
+    def test_get_files_by_extension_include_processed_method(self):
+        temp_dir, file_dir, full_file_name_list = UtilForTesting.file_setup(
+            r'test_5', extension="done",
+            content="col1,col2\ntest1,test2")
+        files = FilesInformation.get_files_by_extension(base_dir=file_dir, extension="csv", include_processed=True)
         self.assertEqual(1, len(files))
 
     def test_get_files_by_extension_include_subdir_include_processed_method(self):
         temp_dir, file_dir, full_file_name_list = UtilForTesting.file_setup(
-            r'test_1\field1=somedata1\field2=somedata2', extension=".done",
+            r'test_6\field1=somedata1\field2=somedata2', extension="",
             content="col1,col2\ntest1,test2")
-        files = FilesInformation.get_files_by_extension(base_dir=file_dir, extension="*.csv", include_subdir=True, include_processed=True)
+        files = FilesInformation.get_files_by_extension(base_dir=temp_dir, extension="csv",
+                                                        include_subdir=True, include_processed=True)
         self.assertEqual(1, len(files))
 
     def test_get_files_excluding_extension_method(self):
-        FilesInformation.get_files_excluding_extension("c:\\", "*.txt")
+        temp_dir, file_dir, full_file_name_list = UtilForTesting.file_setup(
+            r'test_7\field1=somedata1\field2=somedata2', extension="",
+            content="col1,col2\ntest1,test2")
+        files = FilesInformation.get_files_excluding_extension(base_dir=file_dir, extension="csv")
+        self.assertEqual(0, len(files))
 
     def test_get_child_directories_method(self):
         FilesInformation.get_child_directories("c:\\")
--- zeppos_file_manager/files_information.py
+++ zeppos_file_manager/files_information.py
@@ -8,34 +8,39 @@ from os import walk, path
 class FilesInformation:
     @staticmethod
     def get_files_by_extension(base_dir, extension="*", start_file_filter=None, end_file_filter=None,
-                               include_subdir=False, include_processed=False):
+                               include_subdir=False, include_processed=False, exclude_extension=None):
         AppLogger.logger.debug(f'get_files_by_extension: [{base_dir}], [{extension}]')
-        if not include_subdir:
-            file_list = glob(
-                path.join(
-                    base_dir,
-                    f'{str(start_file_filter or "")}*{str(end_file_filter or "")}.{extension}'
-                )
-            )
-        else:
-            file_list = []
-            for root, dirs, files in walk(base_dir):
-                for file in files:
-                    # append the file name to the list
-                    file_list.append(path.join(root, file))
+        file_list = glob(
+            path.join(
+                base_dir,
+                "**" if include_subdir else "",
+                f'{str(start_file_filter or "")}*{str(end_file_filter or "")}.{extension}'
+            ),
+            recursive=include_subdir
+        )
 
         if include_processed:
             for file_marker in FileMarker.file_maker_list():
-                files += FilesInformation.get_files_by_extension(base_dir, f"{extension}.{file_marker}",
-                                                                 start_file_filter, end_file_filter, include_subdir)
+                file_list += FilesInformation.get_files_by_extension(
+                    base_dir=base_dir,
+                    extension=f"{extension}.{file_marker}",
+                    start_file_filter=start_file_filter,
+                    end_file_filter=end_file_filter,
+                    include_subdir=include_subdir,
+                    include_processed=False  # very important. Otherwise we get infinite loop.
+                )
+        if exclude_extension:
+            file_list = filter(lambda x: not x.upper().endswith(f".{exclude_extension.upper()}"), file_list)
 
         return sorted(file_list)
 
     @staticmethod
     def get_files_excluding_extension(base_dir, extension):
         AppLogger.logger.debug(f'get_files: [{base_dir}]')
-        files = glob(path.join(base_dir, '*'))
-        return filter(lambda x: not x.upper().endswith(f".{extension.upper()}"), files)
+        return FilesInformation.get_files_by_extension(
+            base_dir=base_dir,
+            exclude_extension=extension
+        )
 
     @staticmethod
     def get_child_directories(base_dir):
